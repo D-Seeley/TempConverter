1d1f09b212450fade1db00a5ddfb302195993cf6
Daniel Seeley <Daniel.h.seeley@gmail.com>
2018-07-17 19:26:31 -0400

    added .gitignore

--- test/spec.js
+++ test/spec.js
@@ -11,10 +11,12 @@ describe('Temp Converter', ()=> {
     it('has a function to convert C to F', ()=>{
         expect(calc.convertCtoF).to.be.a('function');
         expect(calc.convertCtoF(0)).to.equal(32);
+        expect(calc.convertCtoF(55)).to.equal(131);
     });
 
     it('has a function to convert F to C', ()=>{
         expect(calc.convertFtoC).to.be.a('function');
         expect(calc.convertFtoC(32)).to.equal(0);
+        expect(calc.convertFtoC(131)).to.equal(55);
     });
 });
